6d4a5c7e94ead61f2d224ba77badd314c768d4aa
Nguyễn Trọng Quyết <59854190+Quyet1999@users.noreply.github.com>
2020-10-04 14:21:34 +0700

    Update nhanvien.cs

--- QLNV/QLNV/nhanvien.cs
+++ QLNV/QLNV/nhanvien.cs
@@ -19,8 +19,37 @@ namespace QLNV
             InitializeComponent();
         }
 
-        
-        
+        private void nhanvien_Load(object sender, EventArgs e)
+        {
+
+            load();
+
+        }
+        string chuoiketnoi = @"Data Source = .\SQLEXPRESS; Initial Catalog = QL_NV; Integrated Security = True";
+        SqlConnection conn;
+        private void load()
+        {
+            conn = new SqlConnection(chuoiketnoi);
+            try
+            {
+                //this.nHANVIENTableAdapter.Fill(this.qL_NVDataSet_nhanvien.NHANVIEN);
+                conn.Open();
+               
+                string sql = "select * from NHANVIEN";  // lay het du lieu trong bang 
+                SqlCommand com = new SqlCommand(sql, conn); //bat dau truy van
+                com.CommandType = CommandType.Text;
+                SqlDataAdapter da = new SqlDataAdapter(com); //chuyen du lieu ve
+                DataTable dt = new DataTable(); //tạo một kho ảo để lưu trữ dữ liệu
+                da.Fill(dt);  // đổ dữ liệu vào kho
+                conn.Close();  // đóng kết nối
+                dataGridView1.DataSource = dt; //đổ dữ liệu vào datagridview
+                conn.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("loi ket noi: " + ex.Message);
+            }
+        }
 
         private void label7_Click(object sender, EventArgs e)
         {
@@ -42,16 +71,74 @@ namespace QLNV
             
         }
 
-       
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                DataGridViewRow dr = dataGridView1.Rows[e.RowIndex];
+                tbmanv.Text = dr.Cells[0].Value.ToString();
+                tbtennv.Text = dr.Cells[1].Value.ToString();
+                tbngaysinh.Text = dr.Cells[2].Value.ToString(); // Convert.ToDateTime(dr.Cells[2].Value.ToString());
+                tbgioitinh.Text = dr.Cells[3].Value.ToString();
+                tbtienluong.Text = dr.Cells[4].Value.ToString();
+                tbmaphong.Text = dr.Cells[5].Value.ToString();
+                tbsdt.Text = dr.Cells[6].Value.ToString();
+                tbemail.Text = dr.Cells[7].Value.ToString();
+            }catch( Exception ex)
+            {
+                MessageBox.Show("loi:" + ex.Message);
+            }
+
+
+        }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            
+            SqlConnection conn = new SqlConnection(chuoiketnoi);
+            try
+            {
+                conn.Open();
+                string sql = "update NHANVIEN set HOVN=N'"+ tbtennv.Text +"',NGAYSINH='" +tbngaysinh.Text +"',GT='"+ tbgioitinh.Text + "', LUONG='" + tbtienluong.Text
+                    +"',MAPHONG='"+tbmaphong.Text +"',SDT='"+ tbsdt.Text + "', EMAIL='"+ tbemail.Text + "'where MANV='"+ tbmanv.Text+"'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                int kq = (int)cmd.ExecuteNonQuery();
+                if (kq > 0)
+                {
+                    MessageBox.Show("Sửa Thành Công");
+                    load();
+                }
+                else
+                {
+                    MessageBox.Show("Sửa Thất bại");
+                    conn.Close();
+                }
+            } catch(Exception ex)
+            {
+                MessageBox.Show("loi: " + ex.Message);
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-               
+            DialogResult thongbao;
+            thongbao = MessageBox.Show("Bạn có chắc chắn xóa!!","thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if(thongbao ==DialogResult.OK)
+            {
+                SqlConnection conn = new SqlConnection(chuoiketnoi);
+                try
+                {
+                    conn.Open();
+                    string sql = "delete from NHANVIEN where MANV ='" + tbmanv.Text + "'";
+                    SqlCommand cmd = new SqlCommand(sql,conn);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("XÓA THÀNH CÔNG!");
+                    load();
+                    conn.Close();
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("lỗi kết nối tới CSDL! " + ex.Message);
+                }
+            }    
         }
 
         private void button4_Click(object sender, EventArgs e)
