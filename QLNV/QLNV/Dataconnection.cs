46dcab8a27d77e42469c926db2badfa51a025c78
Be003931 <66206101+Be003931@users.noreply.github.com>
2020-10-29 16:42:51 +0700

    Revert "chỉnh sửa conn"
    This reverts commit 5079826009b113b86ed9f1d187c46925b08175ef.

--- QLNV/QLNV/Dataconnection.cs
+++ QLNV/QLNV/Dataconnection.cs
@@ -1,22 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace QLNV
 {
     class Dataconnection
     {
-        public SqlConnection GetSqlConnection()
-        {
-            return new SqlConnection(@"Data Source=DESKTOP-RQCU70R\SQLEXPRESS;Initial Catalog=QL_NV;Integrated Security=True");
-        }
     }
 }
