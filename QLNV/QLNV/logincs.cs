af1b1c1c293c593fab268d23fb855249134610f2
Sanganh <71274596+Sanganh@users.noreply.github.com>
2020-12-01 09:10:04 +0700

    Revert "Revert "S.A""
    This reverts commit 493d8e6d8cf51a43584abcbd6a437175b828e842.

--- QLNV/QLNV/logincs.cs
+++ QLNV/QLNV/logincs.cs
@@ -14,7 +14,62 @@ namespace QLNV
 {
     public partial class logincs : Form
     {
-        
+        public static string ID_USER = "";
+        public static string USERNAME = "";
+        public static string LEVEL = "";
+        public logincs()
+        {
+            InitializeComponent();
+        }
+        string chuoiketnoi = @"Data Source = .\SQLEXPRESS; Initial Catalog = QL_NV; Integrated Security = True";
+        SqlConnection conn;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string id = "";
+            string level = "";
+            string username = "";
+            try
+            {
+                using (conn = new SqlConnection(chuoiketnoi))
+                {
+                    conn.Open();
+                    string sql = " SELECT CAPDO , MA_ADMIN , USERNAME FROM ADMIN WHERE USERNAME = '" + textBox1.Text + "' and PASSWORD = '" + textBox2.Text +"'";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    if (dt != null)
+                    {
+                        foreach (DataRow dr in dt.Rows)
+                        {
+                            id = dr["MA_ADMIN"].ToString();
+                            ID_USER = id;
+                            username = dr["USERNAME"].ToString();
+                            USERNAME = username;
+                            level = dr["CAPDO"].ToString();
+                            LEVEL = level;
+                        }
+                    }
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read() == true)
+                    {
+                        this.Hide();
+                        Form1 fr1 = new Form1();
+                        fr1.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Thông tin đăng nhập sai");
+                    }
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
 
         private void button2_Click(object sender, EventArgs e)
         {
